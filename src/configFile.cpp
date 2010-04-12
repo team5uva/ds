130f77ea4f16e78edf88f39a9737e06a8ceb561a
Sam Briels <sambrielz@hotmail.com>
2010-04-12 07:44:57 -0700

    Parsing of configuration file working, need to discuss where to store data still

---
+++ src/configFile.cpp
@@ -0,0 +1,94 @@
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+#include "configFile.h"
+
+using namespace std;
+
+string arg_Comment = ("#");
+string arg_Admin = ("$");
+string arg_CSAddress = ("CSAddress ");
+string arg_CSPort = ("CSPort ");
+string arg_listenPort = ("listenPort ");
+string arg_Tag = ("Tag ");
+
+vector<string> adminAccess;
+
+size_t found;
+
+/* Parse supplied configuration file.
+ * Not possible yet to set own path. */
+configFile::ERROR_CODE configFile::parseFile() {
+    string line;
+    ifstream cfgFile("config.cfg");
+    if (cfgFile.is_open()) {
+        while (!cfgFile.eof()) {
+            getline(cfgFile, line);
+            if (line.at(0) == arg_Comment.at(0)) {
+                /* Comment in config file, skipping line. */
+            } else if (line.at(0) == arg_Admin.at(0)) {
+                /* Administrator username and password */
+                if (found = line.find("|")) {
+                    adminAccess.push_back(line.substr(1, found - 1));
+                    adminAccess.push_back(line.substr(
+                            found + 1, line.length() - 1));
+                } else {
+                    return CORRUPT_CONFIG_FILE;
+                }
+            } else {
+                /* Line not a comment, or admin username check if there is any
+                 * info to gather. */
+                if (found = line.find(arg_CSAddress) != string::npos) {
+                    CSAddress = line.substr(
+                            (found + (arg_CSAddress.length() - 1)));
+                }
+                if (found = line.find(arg_CSPort) != string::npos) {
+                    CSPort = convertS2I(line.substr(
+                            found + (arg_CSPort.length() - 1)));
+                }
+                if (found = line.find(arg_listenPort) != string::npos) {
+                    listenPort = convertS2I(line.substr(
+                            found + (arg_listenPort.length() - 1)));
+                }
+                if (found = line.find(arg_Tag) != string::npos) {
+                    Tag = line.substr(found + (arg_Tag.length() - 1));
+                }
+            }
+        }
+        cfgFile.close();
+    } else {
+        return configFile::FILE_NOT_FOUND;
+    }
+    return configFile::SUCCESS;
+}
+
+int configFile::convertS2I(string s) {
+    stringstream ss(s);
+    int j;
+    ss >> j;
+    return j;
+}
+
+int configFile::getListenPort() {
+    return listenPort;
+}
+
+int configFile::getCSPort() {
+    return CSPort;
+}
+
+string configFile::getCSAddress() {
+    return CSAddress;
+}
+
+string configFile::getTag() {
+    return Tag;
+}
+
+vector<string> configFile::getAdmin() {
+    return adminAccess;
+}
---
+++ src/configFile.h
@@ -0,0 +1,36 @@
+#ifndef PARSECONFIG_H_
+#define PARSECONFIG_H_
+
+#include <string>
+
+using namespace std;
+
+class configFile {
+private:
+
+
+public:
+    enum ERROR_CODE {
+        SUCCESS, FAIL, CORRUPT_CONFIG_FILE, COULD_NOT_OPEN_FILE, FILE_NOT_FOUND
+    };
+    string path;
+    int listenPort, CSPort;
+    string CSAddress, Tag;
+
+    configFile() {
+        path = "config.cfg";
+    }
+
+    configFile(string path) {
+        this->path = path;
+    }
+
+    int convertS2I(string s);
+    ERROR_CODE parseFile();
+    int getListenPort();
+    int getCSPort();
+    string getCSAddress();
+    string getTag();
+    vector<string> getAdmin();
+};
+#endif /* PARSECONFIG_H_ */
